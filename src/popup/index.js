bd5a2a4ebfa22588087785300778f8702aa7ad78
HipsterBrown <headhipster@hipsterbrown.com>
2018-03-07 18:28:53 -0500

    feat(popup): update video queue styling, add remove / play actions

--- src/popup/index.js
+++ src/popup/index.js
@@ -16,6 +16,22 @@ app.use((state, emitter) => {
       })
       .catch(console.error);
   });
+
+  emitter.on('queue:remove', ({ index }) => {
+    state.videos = state.videos.filter((_, i) => i !== index);
+    emitter.emit('render');
+
+    browser.storage.sync.set({
+      queue: state.videos,
+    })
+    .catch(console.error);
+  });
+
+  emitter.on('video:open', ({ index, url }) => {
+    browser.tabs.create({ url })
+      .then(() => emitter.emit('queue:remove', { index }))
+      .catch(console.error);
+  });
 });
 
 app.route('*', index);
@@ -26,18 +42,40 @@ function index({videos}, emit) {
   }
   return html`
     <div class="ma3">
-      <h1 class="ma0">Welcome to QueueTube!</h1>
+      <h1 class="ma0 near-black">Welcome to QueueTube!</h1>
       <p>When falling down the rabbit hole of viral videos, just add videos to your queue to have them autoplay.</p>
 
-      <ul id="queue" class="pa0 pl3">${
-        videos ? videos.map(renderVideo) : 'Loading...'
+      <hr class="b--red bw3 ba">
+
+      <ul id="queue" class="list pl0">${
+        videos ? videos.map((video, index) => (
+          renderVideo({
+            ...video,
+            onClick: (event) => {
+              event.preventDefault();
+              emit('video:open', { ...video, index });
+            },
+            onDelete: () => emit('queue:remove', { index }),
+          })
+        )) : 'Loading...'
       }</ul>
     </div>
   `;
 }
 
-function renderVideo({text, url}) {
-  return html`<li><a href=${url} class="red">${text}</a>/li>`;
+const ID_FROM_URL = /watch\?v=(.*)&?/;
+
+function renderVideo({onClick, onDelete, text, url}) {
+  const id = ID_FROM_URL.exec(url)[1];
+  return html`
+    <li class="bb bw1 b--light-silver mb3 hide-child relative">
+      <a href=${url} class="link" onclick=${onClick}>
+        <img src="https://i.ytimg.com/vi/${id}/hqdefault.jpg" alt=${text}>
+        <p class="f4 near-black pv2 ma0 mb2 dim">${text}</p>
+      </a>
+      <span class="child db pointer absolute top-0 right-0 pa2 near-white" onclick=${onDelete}>X Remove</span>
+    </li>
+  `;
 }
 
 app.mount('#app');
